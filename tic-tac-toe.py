a8e110f3a9e4c8d6735d33c384d1d5dfa89a75d0
michael lechner <mlechner2@gmail.com>
2017-11-29 19:52:19 -0700

    adding ignore and cleaning up

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,15 @@
-import sys, cmd
+import cmd
+
 
 class Main(cmd.Cmd):
-    intro = '\nwelcome to tic tac toe! you can enter a move at the prompt for a given symbol using x,y ie: 0,3\n'
+    intro = '\nwelcome to tic tac toe! you can enter a move at the prompt for a given symbol using x,y ie: move 0,3\n'
     symbol = 'X'
     message = '\ntic-tac-toe move for {symbol} '
-    prompt = message.format(symbol = symbol)
-    board = [[None]*3, [None]*3, [None]*3]
+    prompt = message.format(symbol=symbol)
+    board = [[None] * 3, [None] * 3, [None] * 3]
 
-    def do_exit(self, args):
+    @staticmethod
+    def do_exit():
         return True
 
     def do_move(self, args):
@@ -16,7 +18,7 @@ class Main(cmd.Cmd):
         self.print_board(self.board)
 
         if self.is_winner():
-            print '{symbol} has won!\n'.format(symbol = self.symbol)
+            print '{symbol} has won!\n'.format(symbol=self.symbol)
             return True
         else:
             self.update_prompt()
@@ -30,25 +32,27 @@ class Main(cmd.Cmd):
             return True
 
     def is_board_full(self):
-        return len(filter(lambda row: any(cell == None for cell in row), self.board)) == 0
+        return len(filter(lambda row: any(cell is None for cell in row), self.board)) == 0
 
-    def does_sequence_match(self, sequence):
-        if sequence[0] == None: return False
+    @staticmethod
+    def does_sequence_match(sequence):
+        if sequence[0] is None: return False
         return all(x == sequence[0] for x in sequence)
 
     def check_rows(self):
         for row in self.board:
             if self.does_sequence_match(row): return True
-        else: return False
+        else:
+            return False
 
     def check_columns(self):
-        columns = []
         for i, row in enumerate(self.board):
             column = []
             for j, el in enumerate(row):
                 column.append(self.board[j][i])
             if self.does_sequence_match(column): return True
-        else: return False
+        else:
+            return False
 
     def check_diagonals(self):
         right = []
@@ -70,28 +74,32 @@ class Main(cmd.Cmd):
 
     def is_valid_move(self, x, y):
         if x > len(self.board[0]) - 1 or y < 0:
-            print '\nx-coordinate is invalid {x}'.format(x = x)
+            print '\nx-coordinate is invalid {x}'.format(x=x)
             return False
         if y > len(self.board) - 1 or y < 0:
-            print '\ny-coordinate is invalid {y}'.format(y = y)
+            print '\ny-coordinate is invalid {y}'.format(y=y)
             return False
-        if self.board[y][x] == None:
-            print '\nplacing {symbol} to x: {x}, y: {y}'.format(symbol = self.symbol, x = x, y = y)
+        if self.board[y][x] is None:
+            print '\nplacing {symbol} to x: {x}, y: {y}'.format(symbol=self.symbol, x=x, y=y)
             return True
         else:
-            print '\nthis move has already been made x: {x}, y: {y}'.format(x = x, y = y)
+            print '\nthis move has already been made x: {x}, y: {y}'.format(x=x, y=y)
             return False
 
     def update_prompt(self):
-        if self.symbol == 'X': self.symbol = 'O'
-        else: self.symbol = 'X'
-        self.prompt = self.message.format(symbol = self.symbol)
+        if self.symbol == 'X':
+            self.symbol = 'O'
+        else:
+            self.symbol = 'X'
+        self.prompt = self.message.format(symbol=self.symbol)
 
-    def print_board(self, board):
+    @staticmethod
+    def print_board(board):
         print '\n'
         for row in board:
-            print ' | '.join(map(lambda x: x if x != None else ' ', row))
+            print ' | '.join(map(lambda x: x if x is not None else ' ', row))
         print '\n'
+
 
 if __name__ == '__main__':
     Main().cmdloop()
